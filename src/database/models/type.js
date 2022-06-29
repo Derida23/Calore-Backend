bae51872c729c6eeb2759ac6490053f9801915a0
Arian Derida <arianderida@gmail.com>
2022-06-29 09:15:06 +0700

    feat: add new model type_db 🔨

---
+++ src/database/models/type.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Type extends Model {}
+  Type.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.BIGINT(20),
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING(50),
+        field: 'name',
+      },
+      status: {
+        allowNull: false,
+        type: DataTypes.BOOLEAN,
+        defaultValue: 1,
+        field: 'status',
+      },
+      created_at: {
+        allowNull: false,
+        defaultValue: new Date(Date.now()),
+        type: DataTypes.DATE,
+        field: 'created_at',
+      },
+      updated_at: {
+        allowNull: false,
+        defaultValue: new Date(Date.now()),
+        type: DataTypes.DATE,
+        field: 'updated_at',
+      },
+      deleted_at: {
+        allowNull: true,
+        type: DataTypes.DATE,
+        field: 'deleted_at',
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Types',
+      tableName: 'cre_types',
+      underscored: true,
+      freezeTableName: true,
+    }
+  );
+
+  return Type;
+};
